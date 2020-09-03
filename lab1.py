4792815135e140ecdfd84e6fd5de5dbdafef5811
yifeng Shi <yifengshi@yifengs-MacBook-Pro-2.local>
2020-09-03 09:36:27 -0700

    add the lab file

---
+++ lab1.py
@@ -0,0 +1,17 @@
+# This file is to print the welcome message by using python
+
+def print_welcome_message():
+    '''
+    INPUTS: 
+    None
+    
+    OUTPUTS:
+    Welcome Message in String
+
+    This file prints the welcome message
+    '''
+    return "Welcome to DSCI510!"
+
+# Main driver scirpt to execute the function above
+if __name__ == "__main__":
+    print(print_welcome_message())
