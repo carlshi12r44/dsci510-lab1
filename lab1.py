4f3a3a883894e52ecc70f1f4e95b930972256f1b
yifeng Shi <yifengshi@yifengs-MacBook-Pro-2.local>
2020-09-03 09:52:04 -0700

    edit the function to accept message

--- lab1.py
+++ lab1.py
@@ -1,17 +1,17 @@
 # This file is to print the welcome message by using python
 
-def print_welcome_message():
+def print_welcome_message(message):
     '''
     INPUTS: 
-    None
+    message in string
     
     OUTPUTS:
     Welcome Message in String
 
     This file prints the welcome message
     '''
-    return "Welcome to DSCI510!"
+    return message
 
 # Main driver scirpt to execute the function above
 if __name__ == "__main__":
-    print(print_welcome_message())
+    print(print_welcome_message("Welcome to DSCI510!"))
